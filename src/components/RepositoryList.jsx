3d68301ee5320abc8c807b79e38c3c7ac27527bc
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2021-12-21 10:58:45 -0300

    use effect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
+import { useState, useEffect } from "react";
 
 const repository = {
   name: "unform",
@@ -9,6 +10,20 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  /* Dentro do useEffect foi feito um fetch na api do github para buscar os 
+  repositórios, quando esse fetch devolver uma resposta, ela será convertida 
+  para JSON. E quando a resposta para JSON terminar de ser convertida, 
+  teremos os dados do repositório. O próximo passo foi pegar o data e 
+  salvar ele dentro do setRepositories. */
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios:</h1>
