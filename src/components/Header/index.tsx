0c59a2160a01ae9aecf7d93aa720afdf6b7c7003
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-03 16:56:11 -0300

    initial: Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg";
 
 import { Container, Content } from "./styles";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+  /* Se isNewTransactionModalOpen estiver aberto, isso ficará como true 
+  setisNewTransactionModalOpen é para mudar o estado de isNewTransactionModalOpen
+
+  O valor do useState está como falso porque o Modal inicia-se fechado.
+  */
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       {/*Lembrete: logoImg está entre chaves porque é 
       assim que representamos um arquivo JS no HTML!*/}
+
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação!</h2>
+        </Modal>
       </Content>
     </Container>
   );
