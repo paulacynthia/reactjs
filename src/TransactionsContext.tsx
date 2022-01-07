ee5d1274774f576d755f141bc3ba9cb09c815697
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-07 13:33:20 -0300

    moving creation to context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,11 +10,33 @@ interface Transaction {
   createdAt: string;
 }
 
+/*
+interface TransactionInput {
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+}
+*/
+
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
+
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
 interface TransactionsProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[]; // array de transaction
+  createTransaction: (transaction: TransactionInput) => void;
+  /* Uma função que recebe por parâmetro uma transaction do tipo transactionInput que 
+  devolve void (vazio) */
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+); // passa um objeto vazio e força o TS a aceitar que o void tem sim o formato que queremos
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -30,8 +52,18 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     Então, para mostrar que dentro de data, estão os dados usa-se o response e especifica que campo quer apresentar */
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    /* Para o transaction não podemos passar a interface Transaction porque ele esperaria receber o id e a data
+    e quem é responsável por passar isso é o Mirage.JS não o user. Nisso, foi criado outra interface. 
+    */
+    // chamada a api
+    api.post("/transactions", transaction);
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {/* O Contexto não pode só retornar a lista de transactions, ele tem que retornar também a 
+      função de criação. Agora, para acessá-las será necessário fazer uma desestruturação */}
       {children}
     </TransactionsContext.Provider>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -4,7 +4,7 @@ import { Container } from "./styles";
 
 export function TransactionsTable() {
   // O meu useState armazena um array de Transaction
-  const transactions = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <Container>
