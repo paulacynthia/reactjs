c077a4eae6ac54271e217c53c8fdc35e0ca6bb92
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-10 10:26:46 -0300

    stylization with sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>
+        Hello <span>World!</span>
+      </h1>
+      <h2 className={styles.title}>Teste</h2>
+    </>
+  );
 }
