4cd13ec3eebfa4c07a9736f560f6f02e16ed8cc8
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-10 11:43:53 -0300

    global app styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>World!</span>
       </h1>
-      <h2 className={styles.title}>Teste</h2>
     </>
   );
 }
