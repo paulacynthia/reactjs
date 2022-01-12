e4798b4bc8b39fa1b970e0b5360388ecd5e3a1a6
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-12 11:36:47 -0300

    authentication with next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
+      {/*Basicamente, quando der um F5 se o user tá logado ou não chegará ao NextAuthProvider através do pageProps */}
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
--- src/pages/api/users/[id].tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  console.log(request.query);
-  const users = [{ id: 1, name: "Paula" }];
-
-  return response.json(users);
-};
