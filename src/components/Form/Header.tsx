d1f58af23b7308ec21e12578c796ad1732a88880
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-14 14:49:16 -0300

    ending Header

--- src/components/Form/Header.tsx
+++ src/components/Form/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -42,6 +46,34 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" color="gray.400" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Paula Cynthia</Text>
+            <Text color="gray.300" fontSize="small">
+              spaulacynthia@gmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="Paula Cynthia"
+            src="https://github.com/paulacynthia.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
