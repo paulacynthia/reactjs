4f3e412c456dff9777fe1b38795cc9bb4dcc0298
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-17 11:59:57 -0300

    responsive Sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { theme } from "../styles/theme";
 
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SidebarDrawerProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
   );
 }
