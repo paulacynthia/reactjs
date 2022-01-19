0b70d7bf773a99c27bd6f559d199992f74578cfa
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-19 11:35:10 -0300

    settings DevTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "../styles/theme";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
