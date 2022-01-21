59258cdd2f2948c88eb7a3293a65c5b258b4a2fa
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-21 14:31:06 -0300

    request queue in Axios

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = []; // todas as requisições que aconteceram e deram falha
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -21,31 +23,67 @@ api.interceptors.response.use(
         cookies = parseCookies();
 
         const { "nextauth.refreshToken": refreshToken } = cookies;
+        const originalConfig = error.config; // toda config da requisição feita pro back
 
-        api
-          .post("/refresh", {
-            refreshToken,
-          })
-          .then((response) => {
-            const { token } = response.data;
+        // refresh token apenas uma vez independente de quantas chamadas a api aconteçam
+        if (!isRefreshing) {
+          isRefreshing = true;
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
 
-            setCookie(undefined, "nextauth.token", token, {
-              maxAge: 60 * 60 * 24 * 30,
-              path: "/",
-            });
-
-            setCookie(
-              undefined,
-              "nextauth.refreshToken",
-              response.data.refreshToken,
-              {
+              setCookie(undefined, "nextauth.token", token, {
                 maxAge: 60 * 60 * 24 * 30,
-              }
-            );
+                path: "/",
+              });
 
-            //para atualizar o token da api:
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
+              setCookie(
+                undefined,
+                "nextauth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30,
+                }
+              );
+
+              //para atualizar o token da api:
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              /* Se a requisição do post tiver dado certo, vamos lá nas requisições falhadas e para cada uma delas 
+              pega a req e executa o método de sucesso passando o token atualizado e depois deixa a lista em branco. */
+              failedRequestsQueue.forEach((request) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request) => request.onFailure(err));
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+        }
+
+        /* O Axios não aceita trabalhar com async e await, a única forma de fazer ele esperar algo ser 
+        executado é usando Promise e usa resolve e reject para as coisas que você quer que sejam aguardadas */
+
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            onSuccess: (token: string) => {
+              // retentar aquela requisição
+              originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
         // deslogar o user
       }
