56d3efbe4679daefa02787f8e3b37ccf7d5fe374
paulacynthia <paulacynthia.alu.lmb@gmail.com>
2022-01-21 11:34:20 -0300

    retrieving authentication state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { setCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from "nookies";
 import Router from "next/router";
 
 import { api } from "../services/api";
@@ -32,6 +32,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post("sessions", {
@@ -52,6 +64,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
       Router.push("/dashboard");
     } catch (err) {
       console.log(err);
